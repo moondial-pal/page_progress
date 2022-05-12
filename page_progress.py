96965630f9fc2af8bfe7e43d8ef08051bd12304e
moondial <lp@luispal.com>
2022-05-11 22:58:16 -0700

    updated function to ask user for input

--- page_progress.py
+++ page_progress.py
@@ -1,7 +1,26 @@
-def pg_prog(page_number: int, total_pages: int):
+
+# Going to be a Beeware Mobile App potentially
+# Could also make a web app.
+# Could include sqlite db to save progress
+
+""" Incorporate dictionary with uplifting, positive phrases.
+Randomly select phrase from dictionary each time a percentage is printed."""
+
+
+# User must enter total number of pages in book
+def pg_prog(total_pages: int):
+    # Ask user what page they are on
+    page = input("What page are you on?")
+    page_number = int(page)
+
+    # Calculates and creates a percentage value
     percent = page_number/total_pages * 100
     progress = "%g%%" % percent
+
     print("Absolute Legend!! You're at: " + progress)
 
-    # Incorporate dictionary with uplifting, positive phrases.
-    # Randomly select phrase from dictionary each time a percentage is printed.
+
+pg_prog(100)
+
+     Incorporate dictionary with uplifting, positive phrases.
+     Randomly select phrase from dictionary each time a percentage is printed.
